32c1b4c76d1c0ea92551f8956aee4da0f8f0a24b
Pura <pura.acuicultura@gmail.com>
2021-02-24 04:21:25 -0600

    continuation with chapter 4

--- Chapter_4_Diagnostics.R
+++ Chapter_4_Diagnostics.R
@@ -17,6 +17,9 @@ plot(fitted(g),
      ylab = "Residuals")
 abline(h = 0)
 
+fitted(g) # estimated savings rates for each country
+residuals(g) # difference between fitted and actual savings rate for each country
+
 # check for non-constant variance
 plot(fitted(g),
      abs(residuals(g)),
@@ -66,7 +69,70 @@ plot(fitted(gs),
 # NB: square root transformations often appropriate for count response data
 
 # ___.2 Normality (p. 64) ####
+# assess residuals for normality using a qq plot
+qqnorm(residuals(g), ylab = "Residuals")
+qqline(residuals(g))
+
+hist(residuals(g)) # not really a suitable alternative for checking normality
+
+# examples of variation in normality 
+par(mfrow = c(3, 3))
+for(i in 1:9) qqnorm(rnorm(50)) # generally normal
+for(i in 1:9) qqnorm(exp(rnorm(50))) # lognormal/skewed 
+for(i in 1:9) qqnorm(rcauchy(50)) # cauchy, long-tailed platykurtic
+for(i in 1:9) qqnorm(runif(50)) # uniform, short-tailed leptokurtic
+par(mfrow = c(1, 1))
+
+hist(rnorm(50))
+hist(exp(rnorm(50)))
+hist(rcauchy(50))
+hist(runif(50))
+
+# formal test of normality
+shapiro.test(residuals(g)) # Null = residuals are normally distributed
+
 # ___.3 Correlated errors (p. 66) ####
+# note that this section generally referes to serial data (time series)
+data(airquality)
+dim(airquality) # 153 observations of 6 variables
+dim(na.omit(airquality)) # 111 observations of 6 variables
+airquality
+
+pairs(airquality, panel=panel.smooth)
+
+# fit model excluding NA rows
+g <- lm(Ozone ~ Solar.R + Wind + Temp, airquality, na.action = na.exclude)
+summary(g)
+
+# note that non-constant variance and non-linearity can be observed
+plot(fitted(g), residuals(g), xlab="Fitted", ylab="Residuals")
+
+#re-fit the model with log transformation of the solution
+gl <- lm(log(Ozone) ~ Solar.R + Wind + Temp, airquality,na.action=na.exclude)
+
+# note that variance and non-linearity problems are resolved
+plot(fitted(gl), residuals(gl), xlab="Fitted", ylab="Residuals")
+
+# check the residuals for correlation
+plot(residuals(gl), ylab="Residuals")
+abline(h=0)
+
+# plot successive residuals
+plot(residuals(gl)[-153], residuals(gl)[-1], xlab=expression(hat(epsilon)[i]),
+     ylab=expression(hat(epsilon)[i + 1]))
+
+# note how successive residuals were paired
+residuals(gl)[-153] # get rid of last residual
+residuals(gl)[-1] # get rid of first residual
+
+# regression of successive residuals (without intercept - residuals have a mean of 0)
+summary(lm(residuals(gl)[-1] ~ -1 + residuals(gl)[-153]))
+
+# compute the Durbin-Watson statistic (p < 0.05 equals correlation)
+# note that no evidence of correlation is observed
+library(lmtest)
+dwtest(Ozone ~ Solar.R + Wind + Temp, data = na.omit(airquality))
+
 # 4.2 FINDING UNUSUAL OBSERVATIONS (p. 69) #####
 # ___.1 Leverage (p. 69) ####
 # ___.2 Outliers (p. 71) ####
